33e6e6f8c84cb510a923a07cb10486bfef99c9dd
ducnhuan <ducnhuan246@gmail.com>
2019-07-23 17:04:23 +0700

    change access token

--- service/orderService.js
+++ service/orderService.js
@@ -3,8 +3,8 @@ class orderService{
     static getDetail(id)
     {
     var conn = new jsforce.Connection({
-        instanceUrl:'https://testingsvtech-dev-ed.my.salesforce.com',
-        accessToken:'6Cel800D0o0000018x8b8880o000001OvfPnJK0TTXhpvM4M8zEy7E012BqYll1Jp0NeVFQzz9yqjQqc8Sa8ET60p2Jk09eODJ3nWlg2BBX'
+        instanceUrl: 'https://testingsvtech-dev-ed.my.salesforce.com',
+        accessToken: '00D0o0000018x8b!AQsAQKSdhkAF2vJgq18jL3.xLAKi6jiHC0SXCA1srK57y0E1w5YrsCaZmZxCMZsyz0zFvA3KBIbLJ4kB.SiPA1BHFPKBKMle'
     });
     //console.log(id);
     var options={headers:{'Id':id}}; 
